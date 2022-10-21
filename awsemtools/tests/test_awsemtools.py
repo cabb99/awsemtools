e92001c57c2f0f5be0e1856332480199873fff89
Carlos Bueno <cabb99@gmail.com>
2022-10-20 23:36:37 -0500

    Adds sample tests

--- awsemtools/tests/test_awsemtools.py
+++ awsemtools/tests/test_awsemtools.py
@@ -13,3 +13,12 @@ import awsemtools
 def test_awsemtools_imported():
     """Sample test, will always pass so long as import statement worked."""
     assert "awsemtools" in sys.modules
+
+def test_q_value():
+    assert False
+
+def test_strain_pca():
+    assert False
+
+def test_coordinate_pca():
+    assert False
